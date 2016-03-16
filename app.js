e9fe45fd41dff4a5eb19260c5977d57660aa90aa
laivietanh1986 <laivietanh1986@gmail.com>
2016-03-16 23:21:54 +0700

    angular with list

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 /// <reference path="intellisence/angular.d.ts"/>
 var myApp = angular.module('vietAnhNews',[]);
-myApp.controller("MainCtrl",function ($scope) {
-    
+myApp.controller("MainCtrl",['$scope',function ($scope) {
     $scope.test = "Hello World";
-})
+    $scope.posts=
+    ['post 1',
+    'post 2',
+    'post 3',
+    'post 4',
+    'post 5'];
+    
+}]);
