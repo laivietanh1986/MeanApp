ba4fda89ab19910ae0dd1a222cd08444690987d3
laivietanh1986 <laivietanh1986@gmail.com>
2016-03-16 23:08:16 +0700

    init ffile

---
+++ app.js
@@ -0,0 +1,6 @@
+/// <reference path="intellisence/angular.d.ts"/>
+var myApp = angular.module('vietAnhNews',[]);
+myApp.controller("MainCtrl",function ($scope) {
+    
+    $scope.test = "Hello World";
+})
