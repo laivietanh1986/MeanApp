464b6dcd33a6e4ee4eb6e2999c848ad97dfb0578
laivietanh1986 <laivietanh1986@gmail.com>
2016-03-17 22:37:33 +0700

    get user input and adding some style

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 /// <reference path="intellisence/angular.d.ts"/>
 var myApp = angular.module('vietAnhNews',[]);
 myApp.controller("MainCtrl",['$scope',function ($scope) {
-    $scope.test = "Hello World";
-    $scope.posts=
-    ['post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5'];
+    $scope.posts=[
+        {title:'post 1',upvotes:15},
+        {title:'post 2',upvotes:2},
+        {title:'post 3',upvotes:3},
+        {title:'post 4',upvotes:11},
+        {title:'post 5',upvotes:5},
+    ];
+    $scope.addPost = function () {
+        if(!$scope.title || $scope.title ==='') return ;
+        $scope.posts.push({title:$scope.title,upvotes:0,link:$scope.link});
+        $scope.title = '';
+        $scope.link = '';
+    };
+    $scope.incrementUpvotes = function (post) {
+        post.upvotes +=1;
+    }
     
 }]);
